a5ce3514cc2df643898505c7afc277cefe097c75
Gaurav Patil <84006503+g4urav19@users.noreply.github.com>
2022-05-06 20:52:21 +0530

    third commit

---
+++ TexttoHtml/Converter/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+from ckeditor.fields import RichTextField
+
+# Create your models here.
+
+class Post(models.Model):
+    body = RichTextField(blank = True, null = True)
